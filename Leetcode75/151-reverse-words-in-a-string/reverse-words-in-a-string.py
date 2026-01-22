025bf1f34c06b957f85371319948852d46897238
Soumya Johnson <soumyajohnson97@gmail.com>
2026-01-22 11:49:08 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.12%) - LeetSync

---
+++ Leetcode75/151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words=s.split()
+        res=[]
+        for i in range(len(words)-1,-1,-1):
+            res.append(words[i])
+            res.append(" ")
+        res.pop()
+        return ''.join(res)
