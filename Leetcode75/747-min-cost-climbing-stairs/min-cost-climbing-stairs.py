7098e2d22ce68ac43f523952429ebe6e4d8b774d
Soumya Johnson <soumyajohnson97@gmail.com>
2026-02-08 19:31:01 -0500

    Time: 11 ms (5.54%) | Memory: 21.9 MB (5.56%) - LeetSync

---
+++ Leetcode75/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo={}
+        def dp(i):
+            if i<=1:
+                return 0
+            if i in memo:
+                return memo[i]
+            one_step=cost[i-1]+dp(i-1)
+            two_step=cost[i-2]+dp(i-2)
+            memo[i]=min(one_step,two_step)
+            return memo[i]
+        return dp(len(cost))
