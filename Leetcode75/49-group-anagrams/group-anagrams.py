cb6e3abda07237fe849eb4c7ec3da24c9fabc02e
Soumya Johnson <soumyajohnson97@gmail.com>
2026-02-21 17:51:45 -0500

    Time: 11 ms (84.35%) | Memory: 22.1 MB (64.08%) - LeetSync

---
+++ Leetcode75/49-group-anagrams/group-anagrams.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans=defaultdict(list)
+        for s in strs:
+            key="".join(sorted(s))
+            ans[key].append(s)
+        return list(ans.values())
