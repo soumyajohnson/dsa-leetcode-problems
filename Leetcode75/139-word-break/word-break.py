04c77b5f26b9218ca2fa978ee37caf5b288f6dbc
Soumya Johnson <soumyajohnson97@gmail.com>
2026-03-22 22:39:14 -0400

    Time: 6 ms (24.98%) | Memory: 19.2 MB (92.58%) - LeetSync

---
+++ Leetcode75/139-word-break/word-break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo={}
+        def segment(i):
+            if i==len(s):
+                return True
+            if i in memo:
+                return memo[i]
+            for j in range(i+1,len(s)+1):
+                if s[i:j] in wordDict and segment(j):
+                    memo[i]=True
+                    return True
+            memo[i]=False
+            return memo[i]
+        return segment(0)
