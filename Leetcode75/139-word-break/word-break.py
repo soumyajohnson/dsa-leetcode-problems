d755f804426d3a54be176ed52474f34070d1de64
Soumya Johnson <soumyajohnson97@gmail.com>
2026-03-22 22:40:25 -0400

    Time: 3 ms (71.67%) | Memory: 19.3 MB (73.57%) - LeetSync

--- Leetcode75/139-word-break/word-break.py
+++ Leetcode75/139-word-break/word-break.py
@@ -1,13 +1,14 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         memo={}
+        wordDict=set(wordDict)
         def segment(i):
             if i==len(s):
                 return True
             if i in memo:
                 return memo[i]
             for j in range(i+1,len(s)+1):
-                if s[i:j] in set(wordDict) and segment(j):
+                if s[i:j] in wordDict and segment(j):
                     memo[i]=True
                     return True
             memo[i]=False
