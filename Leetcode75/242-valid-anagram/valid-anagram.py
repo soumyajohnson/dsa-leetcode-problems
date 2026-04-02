5cfb592423db13dbcc0fcf256fb6f08d0450a4e0
Soumya Johnson <soumyajohnson97@gmail.com>
2026-04-02 17:35:28 -0400

    Time: 7 ms (90.61%) | Memory: 19.4 MB (59.45%) - LeetSync

--- Leetcode75/242-valid-anagram/valid-anagram.py
+++ Leetcode75/242-valid-anagram/valid-anagram.py
@@ -1,11 +1,5 @@
-from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        smap=defaultdict(int)
-        tmap=defaultdict(int)
-        for c1,c2 in zip(s,t):
-            smap[c1]+=1
-            tmap[c2]+=1
-        return smap==tmap
+        sc=Counter(s)
+        tc=Counter(t)
+        return sc==tc
