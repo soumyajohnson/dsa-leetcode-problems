e63edac55478d25ff767138e61ae1a65608a7379
Soumya Johnson <soumyajohnson97@gmail.com>
2026-04-07 19:28:19 -0400

    Time: 3 ms (75.73%) | Memory: 19.3 MB (55.01%) - LeetSync

--- Leetcode75/1036-rotting-oranges/rotting-oranges.py
+++ Leetcode75/1036-rotting-oranges/rotting-oranges.py
@@ -1,24 +1,26 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         dirs=[[0,1],[1,0],[0,-1],[-1,0]]
-        rows,cols=len(grid),len(grid[0])
-        fresh=0
+        m,n=len(grid),len(grid[0])
+        visited=set()
         q=deque()
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j]==1:
-                    fresh+=1
-                elif grid[i][j]==2:
+        res=0
+        fresh=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==2:
                     q.append((i,j))
-        mins=0
+                elif grid[i][j]==1:
+                    fresh+=1
         while q and fresh>0:
             for _ in range(len(q)):
-                row,col=q.popleft()
-                for dr, dc in dirs:
-                    r,c=row+dr,col+dc
-                    if 0<=r<rows and 0<=c<cols and grid[r][c]==1:
-                        grid[r][c]=2
+                i,j=q.popleft()
+                visited.add((i,j))
+                for r,c in dirs:
+                    dr,dc=i+r,j+c
+                    if 0<=dr<m and 0<=dc<n and (dr,dc) not in visited and grid[dr][dc]==1:
                         fresh-=1
-                        q.append((r,c))
-            mins+=1
-        return mins if fresh==0 else -1
+                        visited.add((dr,dc))
+                        q.append((dr,dc))
+            res+=1
+        return res if fresh==0 else -1
