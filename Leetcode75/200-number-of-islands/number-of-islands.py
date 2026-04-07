813e227ed8a81b87af06bfc0cb42f513155d19a3
Soumya Johnson <soumyajohnson97@gmail.com>
2026-04-07 18:57:33 -0400

    Time: 272 ms (17.68%) | Memory: 31.4 MB (6.06%) - LeetSync

--- Leetcode75/200-number-of-islands/number-of-islands.py
+++ Leetcode75/200-number-of-islands/number-of-islands.py
@@ -1,16 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         dirs=[[0,1],[0,-1],[1,0],[-1,0]]
-        def dfs(i,j):
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]=='0':
-                return
-            grid[i][j]='0'
-            for dr,dc in dirs:
-                dfs(i+dr,j+dc)
+        m,n=len(grid),len(grid[0])
+        seen=set()
         res=0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=='1':
+        def dfs(i,j):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]=="0":
+                return
+            seen.add((i,j))
+            for r,c in dirs:
+                dr,dc=i+r,j+c
+                if 0<=dr<m and 0<=dc<n and (dr,dc) not in seen and grid[dr][dc]=="1":
+                    dfs(dr,dc)
+        for i in range(m):
+            for j in range(n):
+                if (i,j) not in seen and grid[i][j]=="1":
                     res+=1
                     dfs(i,j)
         return res
