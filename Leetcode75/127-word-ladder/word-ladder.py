ff7e5895bd0f03dbb68d17c0e2db74853e242c8a
Soumya Johnson <soumyajohnson97@gmail.com>
2026-04-08 21:23:23 -0400

    Time: 307 ms (33.61%) | Memory: 20.4 MB (73.67%) - LeetSync

--- Leetcode75/127-word-ladder/word-ladder.py
+++ Leetcode75/127-word-ladder/word-ladder.py
@@ -1,18 +1,18 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         wordList=set(wordList)
-        visited=set()
         if endWord not in wordList:
             return 0
         q=deque([(beginWord,1)])
+        visited=set()
         while q:
             curr,l=q.popleft()
             if curr==endWord:
                 return l
             for i in range(len(beginWord)):
                 for c in "abcdefghijklmnopqrstuvwxyz":
-                    new_gene=curr[:i]+c+curr[i+1:]
-                    if new_gene not in visited and new_gene in wordList:
-                        visited.add(new_gene)
-                        q.append((new_gene,l+1))
+                    newword=curr[:i]+c+curr[i+1:]
+                    if newword not in visited and newword in wordList:
+                        visited.add(newword)
+                        q.append((newword,l+1))
         return 0
